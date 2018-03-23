2b2679e0dcd8f8defa06f5136e6e01d9c26b8ffa
Шевченко Игорь Сергеевич <EA-ShevchenkoIS@KC.KPlus>
2018-03-23 18:02:49 +0300

    mlm_revan

--- mlm_revan.py
+++ mlm_revan.py
@@ -1,8 +1,11 @@
 import string
 import re
 from os import listdir
+from collections import Counter
 from nltk.corpus import stopwords
 from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.layers import Dense
 
 def load_doc(filename):
     with open(filename, mode='r') as file:
@@ -19,11 +22,28 @@ def clean_doc(doc):
     tokens = [word for word in tokens if len(word) > 1]
     return tokens
 
+def add_doc_to_vocab(filename, vocab):
+    doc = load_doc(filename)
+    tokens = clean_doc(doc)
+    vocab.update(tokens)
+
 def doc_to_line(filename, vocab):
     doc = load_doc(filename)
     tokens = clean_doc(doc)
     tokens = [w for w in tokens if w in vocab]
     return ' '.join(tokens)
+
+def process_docs_to_vocab(directory, vocab):
+    for filename in listdir(directory):
+        if filename.startswith('cv9'):
+            continue
+        path = directory + '/' + filename
+        add_doc_to_vocab(path, vocab)
+
+def save_list(lines, filename):
+    data = '\n'.join(lines)
+    with open(filename, mode='w') as file:
+        file.write(data)
 
 def process_docs(directory, vocab, is_train):
     lines = list()
@@ -38,10 +58,10 @@ def process_docs(directory, vocab, is_train):
     return lines
 
 def load_clean_dataset(vocab, is_train):
-    neg = process_docs('C:/Users/EA-ShevchenkoIS/Continuum/\
-    anaconda3/MyScripts/Data/txt_sentoken/neg', vocab, is_train)
-    pos = process_docs('C:/Users/EA-ShevchenkoIS/Continuum/\
-    anaconda3/MyScripts/Data/txt_sentoken/pos', vocab, is_train)
+    neg = process_docs((r'C:\Users\EA-ShevchenkoIS\Continuum'+
+    r'\anaconda3\MyScripts\Data\txt_sentoken\neg'), vocab, is_train)
+    pos = process_docs((r'C:\Users\EA-ShevchenkoIS\Continuum'+
+    r'\anaconda3\MyScripts\Data\txt_sentoken\pos'), vocab, is_train)
     docs = neg+pos
     labels = [0 for _ in range(len(neg))] + [1 for _ in range(len(pos))]
     return docs, labels
@@ -50,4 +70,42 @@ def create_tokenizer(lines):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(lines)
     return tokenizer
+
+def define_model(n_words):
+    model = Sequential()
+    model.add(Dense(50, input_shape=(n_words,), activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy'])
+    model.summary()
+    return model
+
+def vocab_setting():
+    vocab = Counter()
+    print('Start word collecting')
+    process_docs_to_vocab((r'C:\Users\EA-ShevchenkoIS\Continuum'+
+    r'\anaconda3\MyScripts\Data\txt_sentoken\pos'), vocab)
+    process_docs_to_vocab((r'C:\Users\EA-ShevchenkoIS\Continuum'+
+    r'\anaconda3\MyScripts\Data\txt_sentoken\neg'), vocab)
+    print('Vocab len: {}'.format(len(vocab)))
+    tokens = [k for k,i in vocab.items() if i >= 2]
+    print('Cleaned vocab len: {}'.format(len(tokens)))
+    save_list(tokens, (r'C:\Users\EA-ShevchenkoIS\Continuum'+
+    r'\anaconda3\MyScripts\Data\vocab.txt'))
+
+def data_preparation():
+    vocab_filename = (r'C:\Users\EA-ShevchenkoIS\Continuum'+
+    r'\anaconda3\MyScripts\Data\vocab.txt')
+    print('Start data preparation')
+    vocab = load_doc(vocab_filename)
+    vocab = set(vocab.split())
+    print('Vocab len: {}'.format(len(vocab)))
+    train_docs, ytrain = load_clean_dataset(vocab, True)
+    test_docs, ytest = load_clean_dataset(vocab, False)
+    tokenizer = create_tokenizer(train_docs)
+    Xtrain = tokenizer.texts_to_matrix(train_docs, mode='freq')
+    Xtest = tokenizer.texts_to_matrix(test_docs, mode='freq')
+    print(Xtrain.shape, Xtest.shape)
+    return [(Xtrain, ytrain), (Xtest, ytest)]
 
