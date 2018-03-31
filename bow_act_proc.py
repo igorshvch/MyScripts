3592971d2b15cb36537c751b1581e3b4bb15de68
Шевченко Игорь Сергеевич <igorshvch@gmail.com>
2018-03-31 15:10:18 +0300

    Bag_of_words model to acts parts

--- bow_act_proc.py
+++ bow_act_proc.py
@@ -1,12 +1,49 @@
-import act_sep as acts
+'import act_sep as acts'
 import re
-from nltk.corpus import stopwords
+import shelve
+'''from nltk.corpus import stopwords
 from collections import Counter
 from keras.preprocessing.text import Tokenize
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense'''
+
+BASE_PATH = r'C:\Users\EA-ShevchenkoIS\Python36\AP\SimpleBase'
+
+def srch_lims(start, stop, inclusion=True):
+    '''
+    The function returns reg.exp object with setted search borders
+    '''
+    if inclusion:
+        pattern = r'(?={0}).+(?<={1})'.format(start, stop)
+    else:
+        pattern = r'(?<={0}).+(?={1})'.format(start, stop)
+    return re.compile(pattern, flags=re.DOTALL)
+
+def extract_databse_info(data_key):
+    '''
+    The function exctracts info from the data base and loads it
+    to memory
+    '''
+    data_base = shelve.open(BASE_PATH)
+    instances = {
+        'tags': sorted(data_base['tags'].keys()),
+        'marked_acts': data_base['marked_acts'].values(),
+        'opened_acts': 0,
+        'acts_keys': 0,
+        'all_acts': 0,
+        'processed_acts': 0
+    }
+    data = instances[data_key]
+    data_base.close()
+    return data
+
+def find_act_part(tag, acts):
+    re_obj = srch_lims(tag, ('/'+tag), inclusion=False)
+    acts_parts = [re_obj.search(act).group(0) for act in acts]
+    return acts_parts
 
 
+'''
 stop_words = set(stopwords.words('russian'))
 Acts = acts.ActSep(string_ending='usual')
 
@@ -31,13 +68,13 @@ tokens = [k for k,i in vocab.items() if i >= 4]
 print(len(tokens))
 
 def clean_acts():
-    new_act = []
     new_store = []
     for act in cln_act_words:
         new_act = [w for w in act if w in tokens]
-        new_store.append(act)
+        new_store.append(new_act)
     return new_store
 
 cleaned_acts = clean_acts()
 tokenizer = Tokenize()
 tokenizer.fit_on_texts(cleaned_acts)
+'''
