ed467c5525da72a3ec4ff2698fc6ef6c0425f5d3
Igor Shevchenko <igorshvch@gmail.com>
2018-04-28 14:28:44 +0300

    recode func added

--- writer.py
+++ writer.py
@@ -29,3 +29,13 @@ def writer(iterable_object, file_name_string, prefix='custome', mode='a'):
         with open(inner_path, mode=mode) as file:
             file.write(iterable_object)
     print('OK')
+
+def recode(root_path, paths, verbose=True):
+    root_path = pthl.Path(root_path)
+    for path in paths:
+        with open(root_path.joinpath(path), mode='r', encoding='koi8_r') as file:
+            text = file.read()
+        with open(root_path.joinpath(path), mode='w', encoding='cp1251') as file:
+            file.write(text)
+        if verbose:
+            print("File '{}' recoded!".format(path))
