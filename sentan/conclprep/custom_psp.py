5a227a4b373b172f10f7f3f46e5f0c6c0ca6dcd6
Igor Shevchenko <igorshvch@gmail.com>
2018-09-26 17:08:48 +0300

    Updates from sentan2_refact

--- sentan/conclprep/custom_psp.py
+++ sentan/conclprep/custom_psp.py
@@ -1,57 +1,147 @@
 import re
 from sentan.gui.dialogs import ffp, fdp, pmb
 from sentan.lowlevel import rwtool
+from collections import deque
 
 __version__ = '0.1'
 
 ###Content=====================================================================
+ARTICLE = 'Статья [0-9]{3}\. .+'
+PARAGRAPH = '[0-9]{1,2}\. .+'
+DESICION_INDEX = '[0-9]{1,2}\.[0-9]{1,2}\. .+'
 DESICION_PURE = '(?<=[0-9]\.[0-9]\. Вывод из судебной практики: ).*'
 DESICION_ALT = (
     '(?<=Вывод из судебной практики: По вопросу о).*(?=у судов нет единой позиции|существует две позиции судов|существует три позиции судов|существует четыре позиции судов|существует пять позиций судов)'
 )
-POSITION = '(?<=Позиция [1-9]\.).*'
+DESICION_CLEANED = (
+    '(?<=По вопросу о).*(?=у судов нет единой позиции|существует две позиции судов|существует три позиции судов|существует четыре позиции судов|существует пять позиций судов)'
+)
+POSITION_DIRTY = 'Позиция [1-9]\. .+'
+POSITION_PURE = '(?<=Позиция [1-9]\.).*'
 
 PATTERNS = {
+    'art': ARTICLE,
+    'par' : PARAGRAPH,
+    'di' : DESICION_INDEX, 
     'dp': DESICION_PURE,
     'da': DESICION_ALT,
-    'p': POSITION
+    'dc': DESICION_CLEANED,
+    'pd': POSITION_DIRTY,
+    'p': POSITION_PURE
 }
 
-def clean_stored_concls():
+def process_concls():
     pmb('Chose file with conclusions!')
-    holder = []
     path_to_file = ffp()
     with open(path_to_file, mode='r') as fle:
         text = fle.read()
-    spl = [line.split('#') for line in text.split('\n') if line]
-    for _, concl in enumerate(spl):
-        if len(concl) == 2:
-            item1, item2 = concl
-            item1 = item1[3:]
-            try:
-                item2 = re.search(PATTERNS['dp'], item2).group().strip(' ')
-            except:
-                print(item2)
-                break
-            holder.append(' '.join([item1, item2]))
-        elif len(concl) == 3:
-            item1, item2, item3 = concl
-            item1 = item1[3:]
-            try:
-                item2 = re.search(PATTERNS['da'], item2).group().strip(' ')
-            except:
-                print('item 2', _, 'part', item2)
-                break
-            try:
-                item3 = re.search(PATTERNS['p'], item3).group().strip(' ')
-            except:
-                print('item 3', _, 'part', item3)
-                break
-            holder.append(' '.join([item1, item2, item3]))
+    spl = [
+        line for line in text.split('\n')
+        if line and not re.match(PATTERNS['art'], line)
+    ]
+    spl = deque(spl)
+    counter = -1
+    flag_del = False
+    holder_current_des = None
+    holder_p = []
+    holder = []
+    while spl:
+        cursor = spl.popleft()
+        counter += 1
+        if re.match(PATTERNS['par'], cursor):
+            flag_del = False
+            holder_p.append(
+                re.match(PATTERNS['par'], cursor).group()
+            )
+        elif re.match(PATTERNS['di'], cursor):
+            flag_del = False
+            holder.append(
+                holder_p[-1] + '#' + re.match(PATTERNS['di'], cursor).group()
+            )
+        elif re.match(PATTERNS['pd'], cursor):
+            if not flag_del:
+                flag_del = True
+                holder_current_des = holder.pop()
+            holder.append(
+                holder_current_des + '#' + re.match(PATTERNS['pd'], cursor).group()
+            )
         else:
-            print(_, len(concl), concl)
-            raise TypeError('Something has gone wrong!')
+            raise ValueError(
+                'String didn\'t match to any mpattern:'
+                +'\n{}, ind: {}'.format(cursor, counter)
+                +'Flags: Del: {}'\
+                .format(flag_del)
+            )
     return holder
+
+def func(cleaned_dec, stored_dec):
+    from writer import writer
+    holder_errors = []
+    holder_res = []
+    cleaned_dec = [line.replace('Вывод из судебной практики: ', '') for line in cleaned_dec]
+    cleaned_dec = [line.replace('\n', '') for line in cleaned_dec]
+    spl_cl = [line.split('#')+['',''] for line in cleaned_dec]
+    print('spl_cl', len(spl_cl))
+    spl_st = [line.split('#')+['',''] for line in stored_dec]
+    print('spl_st', len(spl_st))
+    dct_com =  {(item[0]+item[1].strip('.')).replace(' ', ''):item[0]+'#'+item[1].strip('.') for item in spl_cl}
+    print('dct_com', len(dct_com))
+    spl_cl_keys = [(item[0]+item[1].strip('.')).replace(' ', '') for item in spl_cl]
+    spl_st_keys = [(item[0]+item[1]).replace(' ', '') for item in spl_st]
+    spl_cl = [item[2:] for item in spl_cl]
+    spl_st = [item[2:] for item in spl_st]
+    writer(spl_st_keys, 'spl_st_keys', mode='w')
+    writer(list(dct_com.keys()), 'dct_com_keys', mode='w')
+    flag_fisrt = True
+    for ind, key in enumerate(dct_com.keys()):
+        print(ind, end=' :: ')
+        try:
+            pos_in_st = spl_st_keys.index(key)
+            print ('pos_in_st', pos_in_st,)
+        except:
+            print('error!', end=' == ')
+            holder_errors.append(key)
+            continue
+        pos_in_cl = spl_cl_keys.index(key)
+        if flag_fisrt:
+            pos_in_st+=1
+        while pos_in_st:
+            if flag_fisrt:
+                flag_fisrt = False
+                pos_in_st-=1
+            st = '#'.join([dct_com[key], *spl_st[pos_in_st], *spl_cl[pos_in_cl]])
+            holder_res.append(st)
+            pos_in_st+=1
+            pos_in_cl+=1
+            try:
+                val = spl_st_keys[pos_in_st]
+            except:
+                pos_in_st = None
+            if val != key:
+                break
+    holder_res = [re.subn('#{1,4}', '#', line)[0].rstrip('#') for line in holder_res]
+    return holder_res, holder_errors
+
+def cleaner(string):
+    holder = []
+    spl = [line.lstrip('0123456789. ') for line in string.split('#')]
+    for line in spl:
+        if re.search(PATTERNS['dc'], line):
+            holder.append(re.search(PATTERNS['dc'], line).group())
+        elif re.search(PATTERNS['p'], line):
+            holder.append(re.search(PATTERNS['p'], line).group())
+        else:
+            holder.append(line)
+    return ' '.join(holder)
+        
+
+def clean_string_from_pattern(lst):
+    holder = []
+    for item in lst:
+        res = cleaner(item)
+        holder.append(res)
+    return holder
+
 
 
 ###Testing=====================================================================
---
+++ sentan/conclprep/pspxl.py
@@ -0,0 +1,74 @@
+import re
+from collections import deque
+from sentan.gui.dialogs import ffp, fdp, pmb
+from openpyxl import load_workbook
+
+__version__ = '0.1.2'
+
+###Content=====================================================================
+
+COLS = ('E', 'F', 'AH', 'AJ', 'AK', 'AL', 'AM',)
+KLASS = {
+    'Может быть разворот' : True,
+    'Устоялась есть динамика' : True,
+    'Устоялась нет динамики' : True,
+    'К удалению' : False
+}
+VALS = {'ЦИТАТА АКТА', 'ДОБОР НЕ НУЖЕН', 'ЦИТАТА НОРМЫ', None, '', False}
+
+
+class RowsCollectror():
+    def __init__(self, rows, cols=COLS, break_vals=VALS):
+        self.rows = range(*rows)
+        self.cols = cols
+        self.break_vals = break_vals
+        self.wb=load_workbook(filename=r'C:\Users\EA-ShevchenkoIS\TextProcessing\_TEMP\Аннотация ПСП Поставка 59 выводов.xlsx')
+        self.wsh = 'Лист1'
+    
+    def open_excel(self):
+        pmb('Выберите книгу Excel')
+        self.wb = load_workbook(ffp())
+    
+    def select_sheet(self):
+        print(self.wb.sheetnames)
+        self.wsh = input('Введите название рабочего листа ======>> ')
+    
+    def iterate_over_rows(self):
+        holder = []
+        wsh = self.wb[self.wsh]
+        rows = list(self.rows)
+        cols = self.cols
+        keys = ['par', 'concl', 'klass', 'pos1', 'pos2', 'pos3', 'pos4']
+        for i in rows:
+            cells = [col+str(i) for col in cols]
+            #print(cells)
+            vals = {key:(wsh[cell].value) for key,cell in zip(keys, cells)}
+            #print(vals)
+            if vals['klass'] == 'К удалению':
+                par_concl = vals['par']+'#'+vals['concl']+'#'
+                holder.append(par_concl+'К удалению')
+            else:
+                par_concl = vals['par']+'#'+vals['concl']+'#'
+                holder.append(par_concl+ (vals['pos1'] if vals['pos1'] not in self.break_vals else ''))
+                holder.append(par_concl+ (vals['pos2'] if vals['pos2'] not in self.break_vals else ''))
+                holder.append(par_concl+ (vals['pos3'] if vals['pos3'] not in self.break_vals else ''))
+                holder.append(par_concl+ (vals['pos4'] if vals['pos4'] not in self.break_vals else ''))
+        return holder
+
+
+
+###Testing=====================================================================
+if __name__ == '__main__':
+    import sys
+    try:
+        sys.argv[1]
+        if sys.argv[1] == '-v':
+            print('Module name: {}'.format(sys.argv[0]))
+            print('Version info:', __version__)
+        elif sys.argv[1] == '-t':
+            print('Testing mode!')
+            print('Not implemented!')
+        else:
+            print('Not implemented!')
+    except IndexError:
+        print('Mode var wasn\'t passed!')
